1fbc4fe2d49d4f3a84b1a933b116b20b9905ef65
nikitsya <nikitasmiichyk@gmail.com>
2026-02-16 16:12:02 +0000

    add product table component

--- client/src/components/ProductTable.js
+++ client/src/components/ProductTable.js
@@ -1,3 +1,23 @@
-export class ProductTable {
+import React from "react"
+import {ProductTableRow} from "./ProductTableRow"
 
+
+export const ProductTable = props => {
+    const cars = Array.isArray(props.products) ? props.products : []
+
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th> </th>
+            </tr>
+            </thead>
+            <tbody>
+            {cars.map((product) => <ProductTableRow key={product._id} product={product}/>)}
+            </tbody>
+        </table>
+    )
 }
