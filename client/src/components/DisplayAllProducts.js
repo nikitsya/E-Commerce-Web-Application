00ae9cd673dd48759720763b89a33bd171e7ead7
nikitsya <nikitasmiichyk@gmail.com>
2026-02-16 16:08:53 +0000

    fetch and display products from server

--- client/src/components/DisplayAllProducts.js
+++ client/src/components/DisplayAllProducts.js
@@ -1,3 +1,28 @@
-export class DisplayAllProducts {
+import React, {useState, useEffect} from "react"
+import {Link} from "react-router-dom"
+import axios from "axios"
+import {ProductTable} from "./ProductTable"
+import {SERVER_HOST} from "../config/global_constants"
 
+
+export const DisplayAllProducts = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get(`${SERVER_HOST}/products`)
+            .then(res => {setProducts(Array.isArray(res.data) ? res.data : [])})
+            .catch(() => {setProducts([])})
+            .catch(err => console.log(`${err.response.data}\n${err}`))
+    }, [])
+
+    return (
+        <div className="form-container">
+            <div className="table-container">
+                <ProductTable products={products} />
+                <div className="add-new-product">
+                    <Link to={"/AddProduct"}>Add New Product</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
