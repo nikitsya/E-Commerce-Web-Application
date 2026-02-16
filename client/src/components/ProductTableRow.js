0404454e1829d1f9884b3cc183dbaebb0bee4366
nikitsya <nikitasmiichyk@gmail.com>
2026-02-16 16:15:57 +0000

    add product table row component with edit and delete links

--- client/src/components/ProductTableRow.js
+++ client/src/components/ProductTableRow.js
@@ -1,3 +1,17 @@
-export class ProductTableRow {
+import React from "react"
+import {Link} from "react-router-dom"
 
+
+export const ProductTableRow = props => {
+    return (
+        <tr>
+            <td>{props.product._id}</td>
+            <td>{props.product.product}</td>
+            <td>{props.product.price}</td>
+            <td>
+                <Link className="green-button" to={"/EditProduct/" + props.product._id}>Edit</Link>
+                <Link className="red-button" to={"/DeleteProduct/" + props.product._id}>Delete</Link>
+            </td>
+        </tr>
+    )
 }
