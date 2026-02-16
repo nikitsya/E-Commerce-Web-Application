9fbc4f20213ad5363ce2c3f4ea32876d860f2c4d
nikitsya <nikitasmiichyk@gmail.com>
2026-02-16 19:31:08 +0000

    implement AddProduct

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,3 +1,47 @@
-export class AddProduct {
+import React, {useState} from "react"
+import {Redirect, Link} from "react-router-dom"
+import axios from "axios"
+import {SERVER_HOST} from "../config/global_constants"
 
+
+export const AddProduct = () => {
+    const [name, setName] = useState("")
+    const [price, setPrice] = useState("")
+    const [redirectToDisplayAllProducts, setRedirectToDisplayAllProducts] = useState(false)
+
+    const handleNameChange = e => {
+        setName(e.target.value)
+    }
+
+    const handlePriceChange = e => {
+        setPrice(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const productObject = {
+            name: name.trim(),
+            price: Number(price)
+        }
+
+        axios.post(`${SERVER_HOST}/products`, productObject)
+            .then(() => {setRedirectToDisplayAllProducts(true)})
+            .catch(err => console.log(`${err.response.data}\n${err}`))
+    }
+
+    return (
+        <div className="form-container">{redirectToDisplayAllProducts ? <Redirect to="/DisplayAllProducts"/> : null}
+            <form>
+                <label>Name</label>
+                <input autoFocus type="text" name="name" value={name} onChange={handleNameChange} />
+
+                <label>Price</label>
+                <input type="text" name="price" value={price} onChange={handlePriceChange} />
+
+                <button className="green-button" onClick={handleSubmit}>Add</button>
+                <Link className="red-button" to={"/DisplayAllProducts"}>Cancel</Link>
+            </form>
+        </div>
+    )
 }
