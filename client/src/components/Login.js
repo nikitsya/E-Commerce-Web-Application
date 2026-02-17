c45586c3242d7bfc03c88e8de925757538b1e4c3
nikitsya <nikitasmiichyk@gmail.com>
2026-02-17 21:51:26 +0000

    add Login.js

---
+++ client/src/components/Login.js
@@ -0,0 +1,44 @@
+import React, {useState} from "react"
+import {Redirect, Link} from "react-router-dom"
+import axios from "axios"
+import {Button} from "./Button"
+import {SERVER_HOST} from "../config/global_constants"
+
+export const Login = props => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const handleEmailChange = e => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        axios.post(`${SERVER_HOST}/users/login/${email}/${password}`)
+        .then(() => setIsLoggedIn(true))
+        .catch(err => console.log(`${err.response.data}\n${err}`))
+    }
+
+    return (
+        <form className="form-container" noValidate = {true} id = "loginOrRegistrationForm">
+            <h2>Login</h2>
+
+            {isLoggedIn ? <Redirect to="/DisplayAllCars"/> : null}
+
+            <input type = "email" name = "email" placeholder = "Email" autoComplete="email" autoFocus value={email}
+                onChange={handleEmailChange}
+            /><br/>
+
+            <input type = "password" name = "password" placeholder = "Password" autoComplete="password" value={password}
+                onChange={handlePasswordChange}
+            /><br/><br/>
+
+            <Button value="Login" className="green-button" onClick={handleSubmit}/>
+            <Link className="red-button" to={"/DisplayAllCars"}>Cancel</Link>
+        </form>
+    )
+}
