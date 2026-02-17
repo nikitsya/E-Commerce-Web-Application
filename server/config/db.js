e598860ad55bdb90a10ce646d3f0d4099dda6bae
nikitsya <nikitasmiichyk@gmail.com>
2026-02-17 14:11:09 +0000

    add users route to app

--- server/config/db.js
+++ server/config/db.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
-const dbName = process.env.DB_NAME
-
-if (!dbName) {
-    throw new Error(`DB_NAME is missing. Check server/config/.env`)
-}
-
-mongoose.connect(`mongodb://localhost:27017/${dbName}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`)
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
