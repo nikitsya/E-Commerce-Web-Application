5ef14390d06330f2c86d39ee6bdced4355201fb2
nikitsya <nikitasmiichyk@gmail.com>
2026-02-25 23:45:09 +0000

    Accept cart props and normalize items

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -1,6 +1,8 @@
 import React from "react"
 
-export const ShoppingCart = () => {
+export const ShoppingCart = ({ cartItems, onUpdateQuantity, onRemoveItem, onClearCart }) => {
+    const items = Array.isArray(cartItems) ? cartItems : []
+
     return (
         <div className="form-container">
             <h2>Shopping Cart</h2>
