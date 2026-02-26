bf333c87bd830469ca81d2075f2749d0b581a799
nikitsya <nikitasmiichyk@gmail.com>
2026-02-26 00:15:27 +0000

    pass cart action from ProductTable to row and details modal

--- client/src/components/ProductTable.js
+++ client/src/components/ProductTable.js
@@ -1,33 +1,37 @@
 import React, {useState} from "react"
 import {ProductTableRow} from "./ProductTableRow"
-import { ProductDetailsModal } from "./ProductDetailsModal";
+import {ProductDetailsModal} from "./ProductDetailsModal";
 
 
 export const ProductTable = props => {
     const products = Array.isArray(props.products) ? props.products : []
     const [selectedProduct, setSelectedProduct] = useState(null)
+    const onAddToCart = props.onAddToCart
 
     return (
         <>
-        <table>
-            <thead>
-            <tr>
-                <th>Product Name</th>
-                <th>Price</th>
-                <th>Images</th>
-        
-                <th>Capacity (ml)</th>
-                <th>Material</th>
-                <th>Color</th>
-                <th> </th>
-            </tr>
-            </thead>
+            <table>
+                <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Images</th>
 
-            <tbody>{products.map((product) => <ProductTableRow key={product._id} product={product} onOpenDetails={setSelectedProduct}/>)}</tbody>
-        </table>
+                    <th>Capacity (ml)</th>
+                    <th>Material</th>
+                    <th>Color</th>
+                    <th></th>
+                </tr>
+                </thead>
 
-        <ProductDetailsModal product={selectedProduct} onClose={() => setSelectedProduct(null)}
-        />
+                <tbody>{products.map((product) => <ProductTableRow key={product._id} product={product}
+                                                                   onOpenDetails={setSelectedProduct}
+                                                                   onAddToCart={onAddToCart}/>)}</tbody>
+            </table>
+
+            <ProductDetailsModal product={selectedProduct} onClose={() => setSelectedProduct(null)}
+                                 onAddToCart={onAddToCart}
+            />
         </>
     )
 }
