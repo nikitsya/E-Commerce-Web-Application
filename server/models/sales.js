3aadeb752213ac360b3b1c2dc2d97b8bab2fe511
HannaBProjects <hannabokariuk@gmail.com>
2026-02-28 01:29:51 +0000

    update sales.js:  add guest and logged-in purchase

--- server/models/sales.js
+++ server/models/sales.js
@@ -1,20 +1,27 @@
 const mongoose = require(`mongoose`)
 
 let salesSchema = new mongoose.Schema(
-   {
-         orderID: {type: String, required: true, trim: true},
+    {
+        orderID: {type: String, required: true, trim: true},
         total: {type: Number, required: true, min: 0},
         items: [{
             _id: {type: String, required: true},
-            name: {type: String, required: true},
+            name: {type: String, required: true, trim: true},
             price: {type: Number, required: true, min: 0},
             quantity: {type: Number, required: true, min: 1}
         }],
-        customerName: {type: String,required:true},
-        customerEmail: {type: String,required:true}
-   },
-   {
-       collection: `sales`
-   })
+
+        isGuest: {type: Boolean, required: true, default: false},
+
+        customerName: {type: String, required: true, trim: true},
+        customerEmail: {type: String, required: true, trim: true},
+        customerAddress: {type: String, trim: true, default: ``},
+        customerPhone: {type: String, trim: true, default: ``}
+    },
+    {
+        collection: `sales`,
+        timestamps: true
+    }
+)
 
 module.exports = mongoose.model(`sales`, salesSchema)
