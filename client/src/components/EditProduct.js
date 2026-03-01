c8a30b35aab1626231c07b227a5c0e4ac7b3c57b
nikitsya <nikitasmiichyk@gmail.com>
2026-03-01 06:10:21 +0000

    docs(client): add inline comments to EditProduct flow

--- client/src/components/EditProduct.js
+++ client/src/components/EditProduct.js
@@ -6,14 +6,16 @@ import {buildProductPayload, mapProductToFormValues, useProductForm} from "../ho
 import {ProductFormFields} from "./ProductFormFields"
 
 export const EditProduct = props => {
+    // Shared form hook avoids duplicated handlers across Add/Edit product pages.
     const {formValues, updateField, replaceFormValues} = useProductForm()
+    // Preserve existing route guard behavior for unauthorized users.
     const [redirectToDisplayAllProducts, setRedirectToDisplayAllProducts] = useState(localStorage.accessLevel < ACCESS_LEVEL_CUSTOMER)
-
-
+    
     useEffect(() => {
         //axios.defaults.withCredentials = true // needed for sessions to work
         axios.get(`${SERVER_HOST}/products/${props.match.params.id}`, {headers: {"authorization": localStorage.token}})
             .then((res) => {
+                // Map API product shape to editable text field values.
                 replaceFormValues(mapProductToFormValues(res.data))
             })
             .catch(err => console.log(`${err.response.data}\n${err}`))
@@ -22,6 +24,7 @@ export const EditProduct = props => {
     const handleSubmit = e => {
         e.preventDefault()
 
+        // Normalize and sanitize data before sending update request.
         const productObject = buildProductPayload(formValues)
 
         //axios.defaults.withCredentials = true // needed for sessions to work
