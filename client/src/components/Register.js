4f0fb168d675297335203d189a0cdcc806c0af58
HannaBProjects <hannabokariuk@gmail.com>
2026-03-05 17:25:09 +0000

    import useEffect for preview cleanup

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import {Link, Redirect} from "react-router-dom"
 import axios from "axios"
 import {Button} from "./Button"
