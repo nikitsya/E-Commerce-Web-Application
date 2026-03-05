90ce69bddb7620ae1505ea520572414d2d258c14
HannaBProjects <hannabokariuk@gmail.com>
2026-03-05 09:36:02 +0000

    create EditProfile component scaffold

---
+++ client/src/components/EditProfile.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react"
+import {Link} from "react-router-dom"
+import axios from "axios"
+import {Button} from "./Button"
+import {SERVER_HOST} from "../config/global_constants"
+
+export const EditProfile = () => {
+    return <div>Edit Profile</div>
+}
